c2ddebaee47676a7619a6919d72097fc7097dcb8
Michael Dixon Jr <112347876+michael-dixonjr@users.noreply.github.com>
2023-02-20 08:15:56 -0800

    Architecture & Style Updates

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -1,42 +1,52 @@
-import Logo from "../images/Logo.svg";
+import Logo2 from "../images/Logo2.png";
 import FooterStyles from "./styles/FooterNav.module.css";
 
 function FooterNav() {
   return (
-    <footer>
-    <nav className={FooterStyles.nav}>Doormat Navigation Placeholder
-      <img src={Logo} alt="Logo" />
-      <ul className={FooterStyles.navlinks}>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
+    <footer className={FooterStyles.footercontainer}>
+      <nav className={FooterStyles.nav}>
+        <img className={FooterStyles.footerimg} src={Logo2} alt="Logo" />
+
+        <ul className={FooterStyles.navlist}>
+          <li className={FooterStyles.listtitle}>Navigation</li>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
             <a href="#about">About</a>
-        </li>
-        <li>
+          </li>
+          <li>
             <a href="#menu">Menu</a>
-        </li>
-        <li>
+          </li>
+          <li>
             <a href="#reservations">Reservations</a>
-        </li>
-        <li>
+          </li>
+          <li>
             <a href="#orderonline">Order Online</a>
-        </li>
-        <li>
+          </li>
+          <li>
             <a href="#login">Login</a>
-        </li>
-      </ul>
+          </li>
+        </ul>
+        <ul className={FooterStyles.contactlist}>
+          <li className={FooterStyles.listtitle}>Contact Us</li>
+          <li>Email: info@example.com</li>
+          <li>Phone: +1 555 555-5555</li>
+          <li>Address: 123 Main St, Anytown, USA</li>
+        </ul>
+        <ul className={FooterStyles.sociallist}>
+          <li className={FooterStyles.listtitle}>Socials</li>
+          <li>
+            <a href="#">Facebook</a>
+          </li>
+          <li>
+            <a href="#">Twitter</a>
+          </li>
+          <li>
+            <a href="#">Instagram</a>
+          </li>
+        </ul>
       </nav>
-      <ul className={FooterStyles.contactlist}>Contacts Placeholder
-        <li>Email: info@example.com</li>
-        <li>Phone: +1 555 555-5555</li>
-        <li>Address: 123 Main St, Anytown, USA</li>
-      </ul>
-      <ul className={FooterStyles.sociallist}> Social Links Placeholder
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">Instagram</a></li>
-      </ul>
     </footer>
   );
 }
